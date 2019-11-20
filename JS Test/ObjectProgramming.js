0610a24c5e85231b4f3a7a5a24c1f5838a485aed
Sakenhawk <58000958+Sakenhawk@users.noreply.github.com>
2019-11-20 21:29:30 +0300

    Revert "Revert "Добавили файл ObjectProhgramming.js""
    This reverts commit 239b4691814ea08f608063c7a9f900e289a2fbb8.

---
+++ JS Test/ObjectProgramming.js
@@ -0,0 +1,36 @@
+function Calculator(){
+  this.read = function(){
+    this.a = + prompt("Введите первое число","");
+    this.b = + prompt("Введите второе число","");
+  }
+  this.sum = function(a,b){
+    return this.a + this.b;
+  }
+  this.mul = function(a,b){
+    return this.a * this.b;
+  }
+}
+let calculator = new Calculator();
+calculator.read();
+
+alert( "Sum = " + calculator.sum() );
+alert( "Mul = " + calculator.mul() );
+
+/*
+//Task4 Калькулятор
+let calculator = {
+  read(){
+     this.numb1 = +prompt("Введите первое число");
+     this.numb2 = +prompt("Введите второе число",'');
+      },
+  sum(numb1, numb2){
+    return this.numb1 + this.numb2 ;
+  },
+  mul(){
+    return this.numb1 * this.numb2;
+  }
+};
+calculator.read();
+alert( "Сумма чисел: " + calculator.sum() );
+alert( calculator.mul() );
+*/
